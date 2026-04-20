4a9b57c7ce2721bf1da493bdc3cdb11351b93357
HanByul Yang <hanbyul.yang@gmail.com>
2026-04-20 15:58:26 +0900

    feat: add SupportLinks URL constants for donation channels

---
+++ Sources/CtrlB/SupportLinks.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+/// Destination URLs for the "Support ctrl-b" menu. URLs go live at Stage 1 launch.
+enum SupportLinks {
+    // swiftlint:disable:next force_unwrapping
+    static let githubSponsors = URL(string: "https://github.com/sponsors/yhbyhb")!
+    // swiftlint:disable:next force_unwrapping
+    static let kofi = URL(string: "https://ko-fi.com/yhbyhb")!
+}
